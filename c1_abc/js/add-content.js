bef71654fe895d54d92023a117ea4b75af17572a
“stub4ever” <bui.vquoc@gmail.com>
2015-11-06 12:52:25 +0100

    Fix the bug on javascript

--- c1_abc/js/add-content.js
+++ c1_abc/js/add-content.js
@@ -1,15 +1,22 @@
+//Display the appropriate greeting base on the current time
 
-var today = new Date();
-var hourNow = today.getHours();
+var today = new Date(); //Create a new date object
+var hourNow = today.getHours(); //Find the current hour
 var greeting;
 
 if (hourNow > 18) {
-	greeting = 'Good evening!';
+    greeting = 'Good evening!';
 } else if (hourNow > 12) {
-	greeting = 'Good afternoon!';
+    greeting = 'Good afternoon!';
 } else if (hourNow > 0) {
-	greeting = 'Good morning!';
+    greeting = 'Good morning!';
 } else {
-	greeting = 'Welcome!';
+    greeting = 'Welcome!';
 }
+
+document.write('<h3>' + greeting + '</h3>');
+
+
+
+
 
--- c1_abc/js/min/add-content-min.js
+++ c1_abc/js/min/add-content-min.js
@@ -1,1 +1,1 @@
-var today=new Date,hourNow=today.getHours(),greeting;greeting=hourNow>18?"Good evening!":hourNow>12?"Good afternoon!":hourNow>0?"Good morning!":"Welcome!";
+var today=new Date,hourNow=today.getHours(),greeting;greeting=hourNow>18?"Good evening!":hourNow>12?"Good afternoon!":hourNow>0?"Good morning!":"Welcome!",document.write("<h3>"+greeting+"</h3>");
